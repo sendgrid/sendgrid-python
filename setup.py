a4971b366945f8bc234ec65ba232fa5d26387045
Mike Swift <theycallmeswift@gmail.com>
2012-07-27 11:25:26 -0400

    Bumped to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sendgrid',
-    version='0.1.1',
+    version='0.1.2',
     author='Kane Kim',
     author_email='kane@sendgrid.com',
     packages=find_packages(),
