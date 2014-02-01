4fa594f13c4f34838e61333d53c72d2cc61786e0
elbuo8 <yamil.asusta@sendgrid.com>
2014-02-01 16:38:53 -0500

    Version bump version bump
    pypi docs
    
    rst format

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='sendgrid',
-    version='0.2.0',
+    version='0.2.1',
     author='Yamil Asusta',
     author_email='yamil@sendgrid.com',
     url='https://github.com/sendgrid/sendgrid-python/',
     packages=find_packages(),
-    license='LICENSE.txt',
+    license='MIT',
     description='SendGrid library for Python',
-    long_description='SendGrid library for Python',
+    long_description=open('./README.rst').read(),
     install_requires=[
         'requests',
         'smtpapi'
