7f90879651bb3d0403db3a503ca53041aab1fefb
elbuo8 <yamil.asusta@sendgrid.com>
2014-02-04 11:45:16 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sendgrid',
-    version='0.2.3',
+    version='0.2.4',
     author='Yamil Asusta',
     author_email='yamil@sendgrid.com',
     url='https://github.com/sendgrid/sendgrid-python/',
