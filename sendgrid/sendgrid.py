85159278b37021cb3e3601161589dc16a02f7fbe
Tatiana Gornak <tanja@miumeet.com>
2014-02-22 16:17:52 +0100

    Targeting python 2 and 3 at the same time.

--- sendgrid/sendgrid.py
+++ sendgrid/sendgrid.py
@@ -1,13 +1,11 @@
-import sys
-if sys.hexversion < 0x03000000:
-    # python 2
-    import urllib
-    import urllib2
-else:
-    # python 3
-    import urllib.parse
-    import urllib.request
-
+try:
+    import urllib.request as urllib_request
+    from urllib.parse import urlencode
+    from urllib.error import URLError
+except ImportError: # Python 2
+    import urllib2 as urllib_request
+    from urllib2 import URLError
+    from urllib import urlencode
 
 class SendGridClient(object):
 
@@ -51,36 +49,17 @@ class SendGridClient(object):
         return values
 
     def send(self, message):
-        if sys.hexversion < 0x03000000:
-            # python 2
-            try:
-                if self.proxies:
-                    proxy_support = urllib2.ProxyHandler(self.proxies)
-                    opener = urllib2.build_opener(proxy_support)
-                    urllib2.install_opener(opener)
-                data = urllib.urlencode(
-                    self._build_body(message),
-                    True).encode('utf-8')
-                req = urllib2.Request(self.mail_url, data)
-                response = urllib2.urlopen(req)
-                body = response.read()
-                return response.getcode(), body
-            except urllib2.URLError as e:
-                raise e
-
-        else:
-            # python 3
-            try:
-                if self.proxies:
-                    proxy_support = urllib.request.ProxyHandler(self.proxies)
-                    opener = urllib.request.build_opener(proxy_support)
-                    urllib.request.install_opener(opener)
-                data = urllib.parse.urlencode(
-                    self._build_body(message),
-                    True).encode('utf-8')
-                req = urllib.request.Request(self.mail_url, data)
-                response = urllib.request.urlopen(req)
-                body = response.read()
-                return response.getcode(), body
-            except urllib.error.URLError as e:
-                raise e
+        try:
+            if self.proxies:
+                proxy_support = urllib_request.ProxyHandler(self.proxies)
+                opener = urllib_request.build_opener(proxy_support)
+                urllib_request.install_opener(opener)
+            data = urlencode(
+                self._build_body(message),
+                True).encode('utf-8')
+            req = urllib_request.Request(self.mail_url, data)
+            response = urllib_request.urlopen(req)
+            body = response.read()
+            return response.getcode(), body
+        except URLError as e:
+            raise e
