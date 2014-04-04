2e25582101d21e36176da8c69979277a473ad8e3
Jay Marcyes <jay@marcyes.com>
2014-04-03 17:43:34 -0700

    changes to make it work in 2.6, 2.7, 3.2, and 3.3 python versions

--- test/__init__.py
+++ test/__init__.py
@@ -75,18 +75,18 @@ class TestSendGrid(unittest.TestCase):
     @unittest.skipUnless(sys.version_info < (3, 0), 'only for python2')
     def test__build_body_unicode(self):
         """test _build_body() handles encoded unicode outside ascii range"""
-        from_email = u'\u041f\u0451\u0442\u0440@email.com'.encode('utf-8')
-        from_name = u'\u041f\u0451\u0442\u0440'.encode('utf-8')
-        subject = u'\U00022eb3 \u0445\u043e\u0440\u043e\u0448'.encode('utf-8')
-        text = u'\u0432\u044b\u0439\u0442\u0438 \u043c\u0438'.encode('utf-8')
-        html = u'\u0432\u044b\u0439\u0442\u0438 \u043c\u0438'.encode('utf-8')
-        reply_to = u'\u041f\u0451\u0442\u0440@email.com'.encode('utf-8')
+        from_email = '\xd0\x9d\xd0\xb8\xd0\xba\xd0\xb0@email.com'
+        from_name = '\xd0\x9a\xd0\xbb\xd0\xb0\xd0\xb2\xd0\xb4\xd0\xb8\xd1\x8f'
+        subject = '\xd0\x9d\xd0\xb0\xd0\xb4\xd0\xb5\xd0\xb6\xd0\xb4\xd0\xb0'
+        text = '\xd0\x9d\xd0\xb0\xd0\xb4\xd0\xb5\xd0\xb6\xd0\xb4\xd0\xb0'
+        html = '\xd0\x9d\xd0\xb0\xd0\xb4\xd0\xb5\xd0\xb6\xd0\xb4\xd0\xb0'
+        reply_to = '\xd0\x9d\xd0\xb8\xd0\xba\xd0\xb0@email.com'
         m = Mail()
         m.add_to('John, Doe <john@email.com>')
         m.set_subject(subject)
         m.set_html(html)
         m.set_text(text)
-        m.set_from("{} <{}".format(from_name, from_email))
+        m.set_from("%s <%s>" % (from_name, from_email))
         url = self.sg._build_body(m)
         self.assertEqual(from_email, url['from'])
         self.assertEqual(from_name, url['fromname'])
