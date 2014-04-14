2eb244685d1d129edf32c11cf125b5748587108e
elbuo8 <yamil.asusta@sendgrid.com>
2014-04-14 17:50:03 -0400

    If BCC drop To from XSMTPAPI

--- sendgrid/sendgrid.py
+++ sendgrid/sendgrid.py
@@ -51,6 +51,10 @@ class SendGridClient(object):
                 if isinstance(v, unicode):
                     setattr(message, k, v.encode('utf-8'))
 
+        if (message.bcc) {
+            message.set_tos([]);
+        }
+
         values = {
             'api_user': self.username,
             'api_key': self.password,
