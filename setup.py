690f7f1e325ca278ccd58777ab45a50cda344ee1
Tushar Bhushan <tushar.bhushan@yahoo.com>
2015-09-09 17:52:18 -0700

    changed setup.py add classifers for supported versions issue with python 3.3 and above still remains bumped version
    added classifiers to the setup.py

--- setup.py
+++ setup.py
@@ -25,4 +25,11 @@ setup(
     description='SendGrid library for Python',
     long_description=open('./README.rst').read(),
     install_requires=getRequires(),
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ]
 )
--- test/__init__.py
+++ test/__init__.py
@@ -18,6 +18,7 @@ SG_USER = os.getenv('SG_USER') or 'SENDGRID_USERNAME'
 SG_PWD  = os.getenv('SG_PWD') or 'SENDGRID_PASSWORD'
 
 class TestSendGrid(unittest.TestCase):
+    maxDiff = None
     def setUp(self):
         self.sg = SendGridClient(SG_USER, SG_PWD)
 
@@ -94,7 +95,7 @@ class TestSendGrid(unittest.TestCase):
                 "asm_group_id": 42
             }
             '''))
-        self.assertEqual(url, test_url)
+        self.assertDictEqual(url, test_url)
 
     @unittest.skipUnless(sys.version_info < (3, 0), 'only for python2')
     def test__build_body_unicode(self):
