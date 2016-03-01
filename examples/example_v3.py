292ef997f457c9df1b1bb3d6c445b891f4014afd
Elmer Thomas <elmer@thinkingserious.com>
2016-02-29 22:03:22 -0800

    Moved examples

--- examples/example_v3.py
+++ examples/example_v3.py
@@ -2,11 +2,11 @@ import sendgrid
 import json
 import os
 
-host = "https://e9sk3d3bfaikbpdq7.stoplight-proxy.io"
+host = "http://localhost:4010"
 path = os.path.abspath(os.path.dirname(__file__)) + "/.."
 sg = sendgrid.SendGridAPIClient(host=host, path=path)
 
-data = {"sample": "data", "X-Mock": 200}
+data = {"sample": "data", "X-Mock": 204}
 response = sg.client.asm.suppressions._("global").post(request_headers=data)
 print(response.status_code)
 print(response.response_body)
--- sendgrid/client.py
+++ sendgrid/client.py
@@ -17,13 +17,13 @@ class SendGridAPIClient(object):
         path = "/Users/thinkingserious/Workspace/sendgrid-python"
         python_http_client.Config(path)
         self._apikey = os.environ.get('SENDGRID_API_KEY')
-        self.useragent = 'sendgrid/' + __version__ + ';python_v3'
+        self.useragent = 'sendgrid/{0};python_v3'.format(__version__)
         self.host = opts.get('host', 'https://api.sendgrid.com')
         self.path = opts.get('path', os.path.abspath(os.path.dirname(__file__)))
         self.version = __version__
 
         headers = {
-                    "Authorization": "Bearer " + self._apikey,
+                    "Authorization": 'Bearer {0}'.format(self._apikey),
                     "Content-Type": "application/json",
                     "User-agent": self.useragent
                    }
