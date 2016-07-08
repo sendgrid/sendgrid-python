c3e14e793cf19ee12bd0225bd1dcbb634344d33c
Elmer Thomas <elmer@thinkingserious.com>
2016-07-07 17:07:23 -0700

    Debugging prism startup script

--- test/prism.sh
+++ test/prism.sh
@@ -2,7 +2,7 @@
 
 install () {
 
-set -e
+set -eu
 
 UNAME=$(uname)
 ARCH=$(uname -m)
@@ -28,9 +28,6 @@ fi
 LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
 URL="https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
 DEST=/usr/local/bin/prism
-if [ "$GOPATH" != "" ] ; then
-    DEST=$GOPATH/bin/prism
-fi
 
 if [ -z $LATEST ] ; then
   echo "Error requesting. Download binary from ${URL}"
