e88696023e8ff702ef0d7345b5e5487e70dde91d
Elmer Thomas <elmer@thinkingserious.com>
2016-07-07 17:04:37 -0700

    Don't exit if variable is not set

--- test/prism.sh
+++ test/prism.sh
@@ -2,7 +2,7 @@
 
 install () {
 
-set -eu
+set -e
 
 UNAME=$(uname)
 ARCH=$(uname -m)
@@ -28,10 +28,8 @@ fi
 LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
 URL="https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
 DEST=/usr/local/bin/prism
-if [ -z $GOPATH ]; then
-  if [ "$GOPATH" != "" ] ; then
+if [ "$GOPATH" != "" ] ; then
     DEST=$GOPATH/bin/prism
-  fi
 fi
 
 if [ -z $LATEST ] ; then
