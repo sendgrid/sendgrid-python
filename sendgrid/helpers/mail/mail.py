d80b7d36a0ce3c5ba336cd9bb21a36756deb4fa9
Naveen Pai <naveen.pai@bloomreach.com>
2016-10-11 18:40:24 +0530

    Allow dict to be passed to add_headers
    Currently, mail.add_headers() can only accept an Header object. However,
    many people tend to use {"header": "value"} style dictionaries. This adds
    support for that use case
    
    Resolves #230

--- sendgrid/helpers/mail/mail.py
+++ sendgrid/helpers/mail/mail.py
@@ -117,7 +117,11 @@ class Mail(object):
     def add_header(self, header):
         if self.headers is None:
             self.headers = []
-        self.headers.append(header)
+        if isinstance(header, dict):
+            (k,v) = list(header.items())[0]
+            self.headers.append(Header(k,v))
+        else:
+            self.headers.append(header)
 
     def add_category(self, category):
         if self.categories is None:
