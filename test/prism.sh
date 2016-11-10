759c0188eb61017a3464e34a03b7b3974733fd66
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-11-10 12:39:33 +0200

    Revert to master prism

--- test/prism.sh
+++ test/prism.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 install () {
 
@@ -25,15 +25,16 @@ elif [ "$UNAME" = "Linux" ] ; then
   fi
 fi
 
-LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+#LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+LATEST="v0.1.5"
 URL="https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
-DEST=/usr/local/bin/prism
+DEST=./prism/bin/prism
 
 if [ -z $LATEST ] ; then
   echo "Error requesting. Download binary from ${URL}"
   exit 1
 else
-  curl -sL $URL -o $DEST
+  curl -L $URL -o $DEST
   chmod +x $DEST
 fi
 }
