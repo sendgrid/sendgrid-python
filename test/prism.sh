08d65910b1929e8dd46d207dd37b4b66b3632450
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-11-10 12:33:31 +0200

    Updated prism based on https://raw.githubusercontent.com/stoplightio/prism/master/install.sh

--- test/prism.sh
+++ test/prism.sh
@@ -1,48 +1,41 @@
 #!/bin/sh
 
-install() {
+install () {
 
-    set - eu
+set -eu
 
-    UNAME =$(uname)
-    ARCH =$(uname - m)
-    if ["$UNAME" != "Linux"] & & ["$UNAME" != "Darwin"] & & ["$ARCH" != "x86_64"] & & ["$ARCH" != "i686"]
-    then
+UNAME=$(uname)
+ARCH=$(uname -m)
+if [ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] && [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
     echo "Sorry, OS/Architecture not supported: ${UNAME}/${ARCH}. Download binary from https://github.com/stoplightio/prism/releases"
     exit 1
-    fi
+fi
 
-    if ["$UNAME" = "Darwin"]
-    then
-    OSX_ARCH =$(uname - m)
-    if ["${OSX_ARCH}" = "x86_64"]
-    then
-    PLATFORM = "darwin_amd64"
-    fi
-    elif ["$UNAME" = "Linux"]
-    then
-    LINUX_ARCH =$(uname - m)
-    if ["${LINUX_ARCH}" = "i686"]
-    then
-    PLATFORM = "linux_386"
-    elif ["${LINUX_ARCH}" = "x86_64"]
-    then
-    PLATFORM = "linux_amd64"
-    fi
-    fi
+if [ "$UNAME" = "Darwin" ] ; then
+  OSX_ARCH=$(uname -m)
+  if [ "${OSX_ARCH}" = "x86_64" ] ; then
+    PLATFORM="darwin_amd64"
+  fi
+elif [ "$UNAME" = "Linux" ] ; then
+  LINUX_ARCH=$(uname -m)
+  if [ "${LINUX_ARCH}" = "i686" ] ; then
+    PLATFORM="linux_386"
+  elif [ "${LINUX_ARCH}" = "x86_64" ] ; then
+    PLATFORM="linux_amd64"
+  fi
+fi
 
-    LATEST =$(curl - s https: // api.github.com / repos / stoplightio / prism / tags | grep - Eo '"name":.*?[^\\]",' | head - n 1 | sed 's/[," ]//g' | cut - d ':' - f 2)
-    URL = "https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
-    DEST = /usr / local / bin / prism
+LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+URL="https://github.com/stoplightio/prism/releases/download/$LATEST/prism_$PLATFORM"
+DEST=/usr/local/bin/prism
 
-    if [-z $LATEST]
-    then
-    echo "Error requesting. Download binary from ${URL}"
-    exit 1
-    else
-    curl - sL $URL - o $DEST
-    chmod + x $DEST
-    fi
+if [ -z $LATEST ] ; then
+  echo "Error requesting. Download binary from ${URL}"
+  exit 1
+else
+  curl -sL $URL -o $DEST
+  chmod +x $DEST
+fi
 }
 
 install
