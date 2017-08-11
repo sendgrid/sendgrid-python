fc83b3e0d072cc704f567b01346caf272af1b18a
Elmer Thomas <elmer@thinkingserious.com>
2017-08-11 14:03:54 -0700

    Fix ASCII encoding issue

--- register.py
+++ register.py
@@ -3,7 +3,7 @@ import os
 
 output = pypandoc.convert('README.md', 'rst')
 f = open('README.txt','w+')
-f.write(output)
+f.write(output.encode('utf-8'))
 f.close()
 
 readme_rst = open('./README.txt').read()
@@ -11,4 +11,4 @@ replace = '.. figure:: https://uiux.s3.amazonaws.com/2016-logos/email-logo%402x.
 replacement = '|SendGrid Logo|\n\n.. |SendGrid Logo| image:: https://uiux.s3.amazonaws.com/2016-logos/email-logo%402x.png\n   :target: https://www.sendgrid.com'
 final_text = readme_rst.replace(replace,replacement)
 with open('./README.txt', 'w') as f:
-  f.write(final_text)
+  f.write(final_text.encode('utf-8'))
