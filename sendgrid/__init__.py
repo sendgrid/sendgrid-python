6afcf7d212204730f129c978ef9580dd0b5e07af
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2017-10-22 16:49:43 -0500

    Add top-level pydoc
    Add pydoc for package.  I don't want to duplicate the REAME here, so directing users to it instead.
    
    HTTPS doesn't get highlighted for some reason, so HTTP link used.

--- sendgrid/__init__.py
+++ sendgrid/__init__.py
@@ -1,3 +1,15 @@
+"""
+This library allows you to quickly and easily use the SendGrid Web API v3 via
+Python.  For more information, see the README on Github.
+
+http://github.com/sendgrid/sendgrid-python
+
+Available subpackages
+---------------------
+helpers
+    Modules to help with common tasks.
+"""
+
 from .version import __version__  # noqa
 # v3 API
 from .sendgrid import SendGridAPIClient  # noqa
