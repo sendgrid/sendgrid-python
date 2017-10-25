4f546e15ca6aeae708844d0b8104713a6423194e
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2017-10-25 00:27:51 -0500

    Main docstrings
    Whew, almost done.

--- sendgrid/__init__.py
+++ sendgrid/__init__.py
@@ -1,8 +1,13 @@
 """
 This library allows you to quickly and easily use the SendGrid Web API v3 via
-Python.  For more information, see the README on Github.
+Python.
 
-http://github.com/sendgrid/sendgrid-python
+For more information on this library, see the README on Github.
+    http://github.com/sendgrid/sendgrid-python
+For more information on the SendGrid v3 API, see the v3 docs:
+    http://sendgrid.com/docs/API_Reference/api_v3.html
+For the user guide, code examples, and more, visit the main docs page:
+    http://sendgrid.com/docs/index.html
 
 Available subpackages
 ---------------------
