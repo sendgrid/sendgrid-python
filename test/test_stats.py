f765e4a93f3d1f9288d6374cba16e756453d1866
Aaron Mak Kang Sheng <aaronmakks@gmail.com>
2017-10-27 08:43:37 +0800

    Add more tests

--- test/test_stats.py
+++ test/test_stats.py
@@ -45,6 +45,7 @@ class UnitTests(unittest.TestCase):
     def test_categoryStats(self):
 
         category_stats = CategoryStats(start_date='12-09-2017', categories=['foo', 'bar'])
+        category_stats.add_category(Category('woo'))
         category_stats.end_date = '12-10-2017'
         category_stats.aggregated_by = 'day'
         category_stats._sort_by_direction = 'asc'
@@ -55,7 +56,7 @@ class UnitTests(unittest.TestCase):
             json.dumps(
                 category_stats.get(),
                 sort_keys=True),
-            '{"aggregated_by": "day", "categories": ["foo", "bar"], '
+            '{"aggregated_by": "day", "categories": ["foo", "bar", "woo"], '
             '"end_date": "12-10-2017", "limit": 100, "offset": 2, '
             '"sort_by_direction": "asc", "start_date": "12-09-2017"}'
         )
@@ -63,6 +64,7 @@ class UnitTests(unittest.TestCase):
     def test_subuserStats(self):
 
         subuser_stats = SubuserStats(start_date = '12-09-2017', subusers=['foo', 'bar'])
+        subuser_stats.add_subuser(Subuser('blah'))
         subuser_stats.end_date = '12-10-2017'
         subuser_stats.aggregated_by = 'day'
         subuser_stats._sort_by_direction = 'asc'
@@ -75,5 +77,5 @@ class UnitTests(unittest.TestCase):
                 sort_keys=True),
             '{"aggregated_by": "day", "end_date": "12-10-2017", '
             '"limit": 100, "offset": 2, "sort_by_direction": "asc", '
-            '"start_date": "12-09-2017", "subusers": ["foo", "bar"]}'
+            '"start_date": "12-09-2017", "subusers": ["foo", "bar", "blah"]}'
         )
