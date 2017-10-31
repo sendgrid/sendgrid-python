7b93ad125dedf07afb7eba5120c7ce1f25e48b6f
Kevin Gillette <kevin.gillette@sendgrid.com>
2017-10-31 09:03:57 -0600

    Remove branches, fix travis failure

--- sendgrid/helpers/mail/mail.py
+++ sendgrid/helpers/mail/mail.py
@@ -61,61 +61,27 @@ class Mail(object):
 
         :rtype: dict
         """
-        mail = {}
-        if self.from_email is not None:
-            mail["from"] = self.from_email.get()
-        if self.subject is not None:
-            mail["subject"] = self.subject
+        mail = {
+            'from': _get(self.from_email),
+            'subject': self.subject,
+            'personalizations': _get_list(self.personalizations),
+            'content': _get_list(self.contents),
+            'attachments': _get_list(self.attachments),
+            'template_id': self.template_id,
+            'sections': _merge_dicts(self.sections),
+            'headers': _merge_dicts(self.headers),
+            'categories': _get_list(self.categories),
+            'custom_args': _merge_dicts(self.custom_args),
+            'send_at': self.send_at,
+            'batch_id': self.batch_id,
+            'asm': _get(self.asm),
+            'ip_pool_name': self.ip_pool_name,
+            'mail_settings': _get(self.mail_settings),
+            'tracking_settings': _get(self.tracking_settings),
+            'reply_to': _get(self.reply_to),
+        }
 
-        if self.personalizations is not None:
-            mail["personalizations"] = [
-                personalization.get()
-                for personalization in self.personalizations
-            ]
-
-        if self.contents is not None:
-            mail["content"] = self._get_list(self.contents)
-
-        if self.attachments is not None:
-            mail["attachments"] = self._get_list(self.attachments)
-
-        if self.template_id is not None:
-            mail["template_id"] = self.template_id
-
-        if self.sections is not None:
-            mail["sections"] = self._merge_dicts(self.sections)
-
-        if self.headers is not None:
-            mail["headers"] = self._merge_dicts(self.headers)
-
-        if self.categories is not None:
-            mail["categories"] = self._get_list(self.categories)
-
-        if self.custom_args is not None:
-            mail["custom_args"] = self._merge_dicts(self.custom_args)
-
-        if self.send_at is not None:
-            mail["send_at"] = self.send_at
-
-        if self.batch_id is not None:
-            mail["batch_id"] = self.batch_id
-
-        if self.asm is not None:
-            mail["asm"] = self.asm.get()
-
-        if self.ip_pool_name is not None:
-            mail["ip_pool_name"] = self.ip_pool_name
-
-        if self.mail_settings is not None:
-            mail["mail_settings"] = self.mail_settings.get()
-
-        if self.tracking_settings is not None:
-            mail["tracking_settings"] = self.tracking_settings.get()
-
-        if self.reply_to is not None:
-            mail["reply_to"] = self.reply_to.get()
-
-        return mail
+        return _trim_none(mail)
 
     @property
     def from_email(self):
@@ -180,7 +146,8 @@ class Mail(object):
         This represents a batch of emails sent at the same time. Including a
         batch_id in your request allows you include this email in that batch,
         and also enables you to cancel or pause the delivery of that batch.
-        For more information, see https://sendgrid.com/docs/API_Reference/Web_API_v3/cancel_schedule_send.html
+        For more information, see:
+        https://sendgrid.com/docs/API_Reference/Web_API_v3/cancel_schedule_send.html
 
         :rtype: int
         """
@@ -345,8 +312,8 @@ class Mail(object):
         if self._headers is None:
             self._headers = []
         if isinstance(header, dict):
-            k, v = list(header.items())[0]
-            self._headers.append(Header(k, v))
+            key, val = list(header.items())[0]
+            self._headers.append(Header(key, val))
         else:
             self._headers.append(header)
 
@@ -385,11 +352,29 @@ class Mail(object):
             self._custom_args = []
         self._custom_args.append(custom_arg)
 
-    def _get_list(self, getters):
-        return [x.get for x in getters]
 
-    def _merge_dicts(self, getters):
-        out = {}
-        for key in getters:
-            out.update(key.get())
-        return out
+def _get(getter):
+    if getter is None:
+        return None
+    return getter.get()
+
+
+def _get_list(getters):
+    if getters is None:
+        return None
+    return [x.get() for x in getters]
+
+
+def _merge_dicts(getters):
+    if getters is None:
+        return None
+    out = {}
+    for key in getters:
+        out.update(key.get())
+    return out
+
+
+def _trim_none(dictionary):
+    for key, val in dictionary.items():
+        if val is None:
+            dictionary.pop(key)
