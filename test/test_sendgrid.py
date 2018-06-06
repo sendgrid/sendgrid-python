b75a74620b72614d563198dacc4d09d916680f61
Elmer Thomas <elmer@thinkingserious.com>
2018-06-06 08:28:37 -0700

    Typos

--- test/test_sendgrid.py
+++ test/test_sendgrid.py
@@ -1182,7 +1182,7 @@ class UnitTests(unittest.TestCase):
                             "receiving these emails <% clickhere %>.",
                     "substitution_tag": "<%click here%>",
                     "text": "If you would like to unsubscribe and stop "
-                            "receiveing these emails <% click here %>."
+                            "receiving these emails <% click here %>."
                 }
             }
         }
