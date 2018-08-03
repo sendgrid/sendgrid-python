18936d153c9adbf4fe570e6bc8d23684785ba915
Wojciech Bartosiak <wojtek@flipsports.com>
2018-08-03 17:45:52 +0200

    added tests

--- test/test_mail.py
+++ test/test_mail.py
@@ -562,3 +562,42 @@ class UnitTests(unittest.TestCase):
     def test_directly_setting_substitutions(self):
         personalization = Personalization()
         personalization.substitutions = [{'a': 0}]
+
+    def test_simple_dynamic_template_data(self):
+        """Minimum required to send an email"""
+        mail = Mail()
+
+        mail.from_email = Email("test@example.com")
+
+        mail.subject = "Hello World from the SendGrid Python Library"
+
+        personalization = Personalization()
+        personalization.add_to(Email("test@example.com"))
+        personalization.add_dynamic_template_data({'foo': 'bar'})
+        mail.add_personalization(personalization)
+
+        mail.add_content(Content("text/plain", "some text here"))
+        mail.add_content(
+            Content(
+                "text/html",
+                "<html><body>some text here</body></html>"))
+
+        self.assertEqual(
+            json.dumps(
+                mail.get(),
+                sort_keys=True),
+            '{"content": [{"type": "text/plain", "value": "some text here"}, '
+            '{"type": "text/html", '
+            '"value": "<html><body>some text here</body></html>"}], '
+            '"from": {"email": "test@example.com"}, '
+            '"personalizations": [{'
+                '"dynamic_template_data": {"foo": "bar"}, '
+                '"to": [{"email": "test@example.com"}]'
+            '}], '
+            '"subject": "Hello World from the SendGrid Python Library"'
+            '}'
+        )
+
+        self.assertTrue(isinstance(str(mail), str))
+
+
