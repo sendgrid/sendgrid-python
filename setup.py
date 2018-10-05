91a234c7f34b6856fd62335d5ea3d753ca3819e6
Hugo <hugovk@users.noreply.github.com>
2018-10-05 11:35:14 +0300

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ setup(
     install_requires=getRequires(),
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
