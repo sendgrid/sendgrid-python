4762039541aca8bef1a8f0cca652211a0fcec350
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2018-10-30 10:25:42 +0530

    Updated file with Master branch

--- test/test_sendgrid.py
+++ test/test_sendgrid.py
@@ -1,6 +1,7 @@
 import sendgrid
 from sendgrid.helpers.mail import *
 from sendgrid.version import __version__
+
 try:
     import unittest2 as unittest
 except ImportError:
@@ -25,7 +26,7 @@ class UnitTests(unittest.TestCase):
         cls.sg = sendgrid.SendGridAPIClient(host=host)
         cls.devnull = open(os.devnull, 'w')
         prism_cmd = None
-        
+
         # try:
         #     # check for prism in the PATH
         #     if subprocess.call('prism version'.split(), stdout=cls.devnull) == 0:
@@ -134,7 +135,7 @@ class UnitTests(unittest.TestCase):
         self.assertNotIn('blah', self.sg.client.request_headers)
         self.assertNotIn('blah2x', self.sg.client.request_headers)
 
-        for k,v in self.sg._default_headers.items():
+        for k, v in self.sg._default_headers.items():
             self.assertEqual(v, self.sg.client.request_headers[k])
 
     def test_hello_world(self):
@@ -144,11 +145,11 @@ class UnitTests(unittest.TestCase):
         content = Content(
             "text/plain", "and easy to do anywhere, even with Python")
         mail = Mail(from_email, subject, to_email, content)
-        self.assertTrue(mail.get() == {
-            'content': [{'type': 'text/plain', 'value': 'and easy to do anywhere, even with Python'}],
-            'personalizations': [{'to': [{'email': 'test@example.com'}]}], 'from': {'email': 'test@example.com'},
-            'subject': 'Sending with SendGrid is Fun'
-        })
+        self.assertTrue(
+            mail.get() == {'content': [{'type': 'text/plain', 'value': 'and easy to do anywhere, even with Python'}],
+                           'personalizations': [
+                               {'to': [{'email': 'test@example.com'}]}], 'from': {'email': 'test@example.com'},
+                           'subject': 'Sending with SendGrid is Fun'})
 
     def test_access_settings_activity_get(self):
         params = {'limit': 1}
