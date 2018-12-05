fe2a7edfba13a1fdb45446f888805fa9515225f6
DrMeloMoney <zachmelo@gmail.com>
2018-12-05 11:20:08 -0700

    updating attachment helper

--- sendgrid/helpers/mail/attachment.py
+++ sendgrid/helpers/mail/attachment.py
@@ -1,3 +1,7 @@
+import os 
+import base64
+
+
 class Attachment(object):
     """An attachment to be included with an email."""
 
@@ -9,22 +13,32 @@ class Attachment(object):
         self._disposition = None
         self._content_id = None
 
+
     @property
     def content(self):
         """The Base64 encoded content of the attachment.
-
         :rtype: string
         """
         return self._content
 
     @content.setter
-    def content(self, value):
-        self._content = value
+    def content(self, file_path):
+
+        '''
+        Converts file to base64 encoded string.
+
+        '''
+
+        file = os.path.abspath(file_path)
+        with open(file, 'rb') as file_open:
+             data = file_open.read()
+             data_new = base64.b64encode(data)
+             
+        self._content = data_new
 
     @property
     def type(self):
         """The MIME type of the content you are attaching.
-
         :rtype: string
         """
         return self._type
@@ -36,26 +50,31 @@ class Attachment(object):
     @property
     def filename(self):
         """The filename of the attachment.
-
         :rtype: string
         """
         return self._filename
 
     @filename.setter
-    def filename(self, value):
-        self._filename = value
+    def filename(self, file_name):
+
+        """
+        Converts filename to utf-8
+        """
+
+        encoded_file_name = file_name.encode('utf-8')
+
+
+        self._filename = encoded_file_name 
 
     @property
     def disposition(self):
         """The content-disposition of the attachment, specifying display style.
-
         Specifies how you would like the attachment to be displayed.
          - "inline" results in the attached file being displayed automatically
             within the message.
          - "attachment" results in the attached file requiring some action to
             display (e.g. opening or downloading the file).
         If unspecified, "attachment" is used. Must be one of the two choices.
-
         :rtype: string
         """
         return self._disposition
@@ -67,10 +86,8 @@ class Attachment(object):
     @property
     def content_id(self):
         """The content id for the attachment.
-
         This is used when the disposition is set to "inline" and the attachment
         is an image, allowing the file to be displayed within the email body.
-
         :rtype: string
         """
         return self._content_id
@@ -82,7 +99,6 @@ class Attachment(object):
     def get(self):
         """
         Get a JSON-ready representation of this Attachment.
-
         :returns: This Attachment, ready for use in a request body.
         :rtype: dict
         """
@@ -90,7 +106,7 @@ class Attachment(object):
         if self.content is not None:
             attachment["content"] = self.content
 
-        if self.type is not None:
+        if self.type is not None:   
             attachment["type"] = self.type
 
         if self.filename is not None:
@@ -102,3 +118,4 @@ class Attachment(object):
         if self.content_id is not None:
             attachment["content_id"] = self.content_id
         return attachment
+
