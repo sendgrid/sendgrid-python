f81d63a3866fc20439024af4af26d28143bf3a04
G-Cracker <54359760+G-Cracker@users.noreply.github.com>
2019-10-27 11:42:18 +0300

    Create sendgrid.py

---
+++ sendgrid.py
@@ -0,0 +1,20 @@
+
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='godwinmuthomim07@gmail.com',
+    to_emails='to@example.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
