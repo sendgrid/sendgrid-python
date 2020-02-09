75e9a099fe7179468df196971a0c89e37e5d1f79
acctonchuck <acctonchuck@users.noreply.github.com>
2020-02-09 18:35:05 -0500

    pip install sendgrid

--- sendgrid/version.py
+++ sendgrid/version.py
@@ -1,1 +1,4 @@
 __version__ = '6.1.1'
+echo "export SENDGRID_API_KEY='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' " > sendgrid.env
+echo "sendgrid.env" >> .gitignore
+source ./sendgrid.env
