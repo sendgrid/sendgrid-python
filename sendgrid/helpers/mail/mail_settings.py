620f299c8ca2ade47ae6c6e19f5be89735d7c53a
Alex Narayanan <alex.narayanan@klaviyo.com>
2021-03-25 17:17:35 -0400

    Add v3 suppression bypass filters to the MailSettings object
    - bypass_spam_management
    - bypass_bounce_management
    - bypass_unsubscribe_management

--- sendgrid/helpers/mail/mail_settings.py
+++ sendgrid/helpers/mail/mail_settings.py
@@ -3,7 +3,10 @@ class MailSettings(object):
 
     def __init__(self,
                  bcc_settings=None,
+                 bypass_bounce_management=None,
                  bypass_list_management=None,
+                 bypass_spam_management=None,
+                 bypass_unsubscribe_management=None,
                  footer_settings=None,
                  sandbox_mode=None,
                  spam_check=None):
@@ -11,9 +14,18 @@ class MailSettings(object):
 
         :param bcc_settings: The BCC Settings of this MailSettings
         :type bcc_settings: BCCSettings, optional
+        :param bypass_bounce_management: Whether this MailSettings bypasses bounce management.
+                                         Should not be combined with bypass_list_management.
+        :type bypass_list_management: BypassBounceManagement, optional
         :param bypass_list_management: Whether this MailSettings bypasses list
                                        management
         :type bypass_list_management: BypassListManagement, optional
+        :param bypass_spam_management: Whether this MailSettings bypasses spam management.
+                                       Should not be combined with bypass_list_management.
+        :type bypass_list_management: BypassSpamManagement, optional
+        :param bypass_unsubscribe_management: Whether this MailSettings bypasses unsubscribe management.
+                                              Should not be combined with bypass_list_management.
+        :type bypass_list_management: BypassUnsubscribeManagement, optional
         :param footer_settings: The default footer specified by this
                                 MailSettings
         :type footer_settings: FooterSettings, optional
@@ -24,7 +36,10 @@ class MailSettings(object):
         :type spam_check: SpamCheck, optional
         """
         self._bcc_settings = None
+        self._bypass_bounce_management = None
         self._bypass_list_management = None
+        self._bypass_spam_management = None
+        self._bypass_unsubscribe_management = None
         self._footer_settings = None
         self._sandbox_mode = None
         self._spam_check = None
@@ -32,8 +47,17 @@ class MailSettings(object):
         if bcc_settings is not None:
             self.bcc_settings = bcc_settings
 
+        if bypass_bounce_management is not None:
+            self.bypass_bounce_management = bypass_bounce_management
+
         if bypass_list_management is not None:
             self.bypass_list_management = bypass_list_management
+
+        if bypass_spam_management is not None:
+            self.bypass_spam_management = bypass_spam_management
+
+        if bypass_unsubscribe_management is not None:
+            self.bypass_unsubscribe_management = bypass_unsubscribe_management
 
         if footer_settings is not None:
             self.footer_settings = footer_settings
@@ -62,6 +86,23 @@ class MailSettings(object):
         self._bcc_settings = value
 
     @property
+    def bypass_bounce_management(self):
+        """Whether this MailSettings bypasses bounce management.
+
+        :rtype: BypassBounceManagement
+        """
+        return self._bypass_bounce_management
+
+    @bypass_bounce_management.setter
+    def bypass_bounce_management(self, value):
+        """Whether this MailSettings bypasses bounce management.
+
+        :param value: Whether this MailSettings bypasses bounce management.
+        :type value: BypassBounceManagement
+        """
+        self._bypass_bounce_management = value
+
+    @property
     def bypass_list_management(self):
         """Whether this MailSettings bypasses list management.
 
@@ -77,6 +118,40 @@ class MailSettings(object):
         :type value: BypassListManagement
         """
         self._bypass_list_management = value
+
+    @property
+    def bypass_spam_management(self):
+        """Whether this MailSettings bypasses spam management.
+
+        :rtype: BypassSpamManagement
+        """
+        return self._bypass_spam_management
+
+    @bypass_spam_management.setter
+    def bypass_spam_management(self, value):
+        """Whether this MailSettings bypasses spam management.
+
+        :param value: Whether this MailSettings bypasses spam management.
+        :type value: BypassSpamManagement
+        """
+        self._bypass_spam_management = value
+
+    @property
+    def bypass_unsubscribe_management(self):
+        """Whether this MailSettings bypasses unsubscribe management.
+
+        :rtype: BypassUnsubscribeManagement
+        """
+        return self._bypass_unsubscribe_management
+
+    @bypass_unsubscribe_management.setter
+    def bypass_unsubscribe_management(self, value):
+        """Whether this MailSettings bypasses unsubscribe management.
+
+        :param value: Whether this MailSettings bypasses unsubscribe management.
+        :type value: BypassUnsubscribeManagement
+        """
+        self._bypass_unsubscribe_management = value
 
     @property
     def footer_settings(self):
@@ -141,9 +216,21 @@ class MailSettings(object):
         if self.bcc_settings is not None:
             mail_settings["bcc"] = self.bcc_settings.get()
 
+        if self.bypass_bounce_management is not None:
+            mail_settings[
+                "bypass_bounce_management"] = self.bypass_bounce_management.get()
+
         if self.bypass_list_management is not None:
             mail_settings[
                 "bypass_list_management"] = self.bypass_list_management.get()
+
+        if self.bypass_spam_management is not None:
+            mail_settings[
+                "bypass_spam_management"] = self.bypass_spam_management.get()
+
+        if self.bypass_unsubscribe_management is not None:
+            mail_settings[
+                "bypass_unsubscribe_management"] = self.bypass_unsubscribe_management.get()
 
         if self.footer_settings is not None:
             mail_settings["footer"] = self.footer_settings.get()
