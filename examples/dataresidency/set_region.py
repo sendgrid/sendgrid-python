2a9930f28c086e34e544d530c7a45bedd841147d
manisha1997 <singhmanisha.250025@gmail.com>
2023-11-16 13:47:12 +0530

    feat: geolocation setter in sendgrid-python for GDPR compliance

--- examples/dataresidency/set_region.py
+++ examples/dataresidency/set_region.py
@@ -7,13 +7,13 @@ from sendgrid import Email, To, Content, Mail
 # setting region to be "global"
 # sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 
-sg = sendgrid.SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 sg.set_data_residency("global")
 from_email = Email("example@abc.com")
 to_email = To("example@abc.com")
 subject = "Sending with SendGrid is Fun"
 content = Content("text/plain", "and easy to do anywhere, even with Python")
-print(sg.host)
+print(sg.client.host)
 mail = Mail(from_email, to_email, subject, content)
 response = sg.client.mail.send.post(request_body=mail.get())
 print(response)
@@ -23,15 +23,30 @@ print(response.headers)
 
 
 # Example 2
-# setting region to "eu"
-sg = sendgrid.SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+# setting host
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 sg.set_host("https://api.eu.sendgrid.com")
-# sg.set_data_residency("eu")
 from_email = Email("example@abc.com")
 to_email = To("example@abc.com")
 subject = "Sending with SendGrid is Fun"
 content = Content("text/plain", "and easy to do anywhere, even with Python")
-print(sg.host)
+print(sg.client.host)
+mail = Mail(from_email, to_email, subject, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response)
+print(response.status_code)
+print(response.body)
+print(response.headers)
+
+# Example 3
+# setting region to "eu"
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+sg.set_data_residency("eu")
+from_email = Email("example@abc.com")
+to_email = To("example@abc.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+print(sg.client.host)
 mail = Mail(from_email, to_email, subject, content)
 response = sg.client.mail.send.post(request_body=mail.get())
 print(response)
