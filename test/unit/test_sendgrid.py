7eb07f72b5dd38b4ce010a7b573becfa5cff22e2
manisha1997 <singhmanisha.250025@gmail.com>
2023-11-22 14:15:50 +0530

    feat: geolocation setter in sendgrid-python for GDPR compliance

--- test/unit/test_sendgrid.py
+++ test/unit/test_sendgrid.py
@@ -8,20 +8,20 @@ class UnitTests(unittest.TestCase):
 
     def test_host_with_eu_region(self):
         sg = sendgrid.SendGridAPIClient(api_key='MY_API_KEY')
-        sg.set_data_residency("eu")
+        sg.set_sendgrid_data_residency("eu")
         self.assertEqual("https://api.eu.sendgrid.com",sg.client.host)
 
     def test_host_with_global_region(self):
         sg = sendgrid.SendGridAPIClient(api_key='MY_API_KEY')
-        sg.set_data_residency("global")
+        sg.set_sendgrid_data_residency("global")
         self.assertEqual("https://api.sendgrid.com",sg.client.host)
 
     def test_with_region_is_none(self):
         sg = sendgrid.SendGridAPIClient(api_key='MY_API_KEY')
         with self.assertRaises(ValueError):
-            sg.set_data_residency(None)
+            sg.set_sendgrid_data_residency(None)
 
     def test_with_region_is_invalid(self):
         sg = sendgrid.SendGridAPIClient(api_key='MY_API_KEY')
         with self.assertRaises(ValueError):
-            sg.set_data_residency("abc")
+            sg.set_sendgrid_data_residency("abc")
