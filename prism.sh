91320e0a38b40007d73bd14c799156e270c9a95b
Shubham Tiwari <stiwari@twilio.com>
2025-09-11 13:04:14 +0530

    chore: add prism file

--- prism.sh
+++ prism.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf prism && mkdir -p prism && cd prism
-git clone --depth 1 https://${GITHUB_TOKEN}@github.com/sendgrid/sendgrid-oai .
+git clone --depth 1 https://github.com/sendgrid/sendgrid-oai .
 cd prism
 
 docker-compose build --parallel
